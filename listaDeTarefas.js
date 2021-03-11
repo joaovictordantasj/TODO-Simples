4de90818dcb022e1897d2426b563b3c9a2643733
João Victor <joaovictordjardim@gmail.com>
2021-03-11 11:07:41 -0300

    Funcao de concluir tarefa criada

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,19 +1,42 @@
-const criarTarefa = (e) => {
-  e.preventDefault();
-  
-  const lista = document.querySelector('[data-list]')
-  const input = document.querySelector('[data-form-input]');  
-  const valor = input.value;  
+(() => {
+  const criarTarefa = (e) => {
+    e.preventDefault();
+    
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');  
+    const valor = input.value;  
 
-  const tarefa = document.createElement('li');
-  tarefa.classList.add('task')
-  const conteudo = `<p class="content">${valor}</p>`;
-  
-  tarefa.innerHTML = conteudo;
-  lista.appendChild(tarefa);
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+    
+    tarefa.innerHTML = conteudo;
+    
+    tarefa.appendChild(BotaoConlui());
+    lista.appendChild(tarefa);
 
-  input.value = '';
-}
+    input.value = '';
+  }
 
-const novaTarefa = document.querySelector('[data-form-button]');
-novaTarefa.addEventListener('click', criarTarefa);
+  const novaTarefa = document.querySelector('[data-form-button]');
+  novaTarefa.addEventListener('click', criarTarefa);
+
+  const BotaoConlui = () => {
+    const botaoConlui = document.createElement('button');
+
+    botaoConlui.classList.add('check-button');
+    botaoConlui.innerText = 'Concluir';
+
+    botaoConlui.addEventListener('click', concluirTarefa);
+
+    return botaoConlui;
+  }
+
+  const concluirTarefa = (e) => {
+    const botaoConlui = e.target;
+
+    const tarefaCompleta = botaoConlui.parentElement;
+
+    tarefaCompleta.classList.toggle('done');
+  }
+})();
